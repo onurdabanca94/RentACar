bc2b8d7677073901d78e9997f9b158e868b89ca6
Onur Dabanca <onurdbnc@gmail.com>
2024-06-04 04:05:53 +0300

    User rules and commands created.

---
+++ Application/Features/Users/Commands/Create/CreatedUserResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace Application.Features.Users.Commands.Create;
+
+public class CreatedUserResponse
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Email { get; set; }
+    public bool Status { get; set; }
+
+    public CreatedUserResponse()
+    {
+        Firstname = string.Empty;
+        Lastname = string.Empty;
+        Email = string.Empty;
+    }
+
+    public CreatedUserResponse(int id, string firstName, string lastName, string email, bool status)
+    {
+        Id = id;
+        Firstname = firstName;
+        Lastname = lastName;
+        Email = email;
+        Status = status;
+    }
+}
